831341c8bebc0ad719dcd2e6852eb36e78c4b7fd
yuya uchida <u.yuya0616ll@gmail.com>
2021-01-24 21:56:56 +0900

    blogsコントローラーの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'blogs/index'
+  resources :blogs
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
 end

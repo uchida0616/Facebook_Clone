06a8e5564a8169a6b22489d1bfa0e28baffd5095
yuya uchida <u.yuya0616ll@gmail.com>
2021-01-24 17:28:53 +0900

    ログイン中のuser名表示を追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,13 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      redirect_to user_path(user.id)
+    else
+      flash.now[:danger] = 'ログインに失敗しました'
+      render :new
+    end
   end
 end

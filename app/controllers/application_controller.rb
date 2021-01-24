feda0b14b791c789d176878f01e7ccf964a8b780
yuya uchida <u.yuya0616ll@gmail.com>
2021-01-24 23:21:15 +0900

    ブログ一覧画面を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,5 @@ class ApplicationController < ActionController::Base
       flash[:notice] = t('notice.login_needed')
       redirect_to new_session_path
     end
+  end
 end
--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,4 +1,23 @@
 class BlogsController < ApplicationController
   def index
+    @blogs = Blog.all
+  end
+
+  def new
+    @blog = Blog.new
+  end
+
+  def create
+    Blog.create(blog_params)
+    redirect_to new_blog_path
+  end
+
+  def show
+    @blog = Blog.find(params[:id])
+  end
+
+  private
+  def blog_params
+    params.require(:blog).permit(:title, :content)
   end
 end

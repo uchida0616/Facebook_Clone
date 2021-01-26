145ecba28f137cf7ff3648287a72341180fd52a1
yuya uchida <u.yuya0616ll@gmail.com>
2021-01-26 23:28:39 +0900

    bootstrapを導入

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,7 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require activestorage
 //= require turbolinks
+//= require jquery/dist/jquery.js
 //= require_tree .
+//= require bootstrap/dist/js/bootstrap.min
